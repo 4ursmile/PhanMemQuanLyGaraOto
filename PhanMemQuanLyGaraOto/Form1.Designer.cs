bf1999e58f6665cd6f0425709efbb0f7748a36b6
BaoLy <21521109@gm.uit.edu.vn>
2022-11-11 20:30:36 +0700

    Add project files.

---
+++ PhanMemQuanLyGaraOto/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace PhanMemQuanLyGaraOto
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.kryptonPalette1 = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Loichucdau = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserNameText = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.PassWordText = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SigninButton = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kryptonPalette1
+            // 
+            this.kryptonPalette1.ButtonSpecs.FormClose.Image = global::PhanMemQuanLyGaraOto.Properties.Resources.FormExit;
+            this.kryptonPalette1.ButtonSpecs.FormClose.ImageStates.ImageNormal = global::PhanMemQuanLyGaraOto.Properties.Resources.FormExit;
+            this.kryptonPalette1.ButtonSpecs.FormClose.ImageStates.ImagePressed = global::PhanMemQuanLyGaraOto.Properties.Resources.FormExit;
+            this.kryptonPalette1.ButtonSpecs.FormClose.ImageStates.ImageTracking = global::PhanMemQuanLyGaraOto.Properties.Resources.FormExitHover;
+            this.kryptonPalette1.ButtonSpecs.FormClose.Style = ComponentFactory.Krypton.Toolkit.PaletteButtonStyle.Command;
+            this.kryptonPalette1.ButtonSpecs.FormMax.Image = global::PhanMemQuanLyGaraOto.Properties.Resources.FormMaximize;
+            this.kryptonPalette1.ButtonSpecs.FormMin.Image = global::PhanMemQuanLyGaraOto.Properties.Resources.FormMinimize;
+            this.kryptonPalette1.ButtonSpecs.FormMin.ImageStates.ImageNormal = global::PhanMemQuanLyGaraOto.Properties.Resources.FormMinimize;
+            this.kryptonPalette1.ButtonSpecs.FormMin.ImageStates.ImagePressed = global::PhanMemQuanLyGaraOto.Properties.Resources.FormMinimize;
+            this.kryptonPalette1.ButtonSpecs.FormMin.ImageStates.ImageTracking = global::PhanMemQuanLyGaraOto.Properties.Resources.FormMinimizeHover;
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Back.Color1 = System.Drawing.Color.Lime;
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Border.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Border.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonPalette1.ButtonStyles.ButtonForm.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.StateTracking.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonPalette1.ButtonStyles.ButtonFormClose.StateTracking.Back.Color2 = System.Drawing.Color.Red;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.Color2 = System.Drawing.SystemColors.Menu;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.None;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.ImageAlign = ComponentFactory.Krypton.Toolkit.PaletteRectangleAlign.Form;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.ImageStyle = ComponentFactory.Krypton.Toolkit.PaletteImageStyle.Stretch;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.Rounding = 12;
+            this.kryptonPalette1.FormStyles.FormMain.StateCommon.Border.Width = 0;
+            this.kryptonPalette1.HeaderStyles.HeaderCommon.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.HeaderStyles.HeaderCommon.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.kryptonPalette1.HeaderStyles.HeaderCommon.StateCommon.Border.Color1 = System.Drawing.Color.Black;
+            this.kryptonPalette1.HeaderStyles.HeaderCommon.StateCommon.Border.Color2 = System.Drawing.Color.Black;
+            this.kryptonPalette1.HeaderStyles.HeaderCommon.StateCommon.Border.DrawBorders = ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom;
+            this.kryptonPalette1.HeaderStyles.HeaderCommon.StateCommon.ButtonEdgeInset = 10;
+            this.kryptonPalette1.HeaderStyles.HeaderCommon.StateCommon.ButtonPadding = new System.Windows.Forms.Padding(-1, 5, 1, -1);
+            this.kryptonPalette1.HeaderStyles.HeaderCommon.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, 10, 1, -1);
+            this.kryptonPalette1.PalettePaint += new System.EventHandler<ComponentFactory.Krypton.Toolkit.PaletteLayoutEventArgs>(this.kryptonPalette1_PalettePaint);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::PhanMemQuanLyGaraOto.Properties.Resources.giphy;
+            this.pictureBox2.Location = new System.Drawing.Point(606, -10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(254, 185);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PhanMemQuanLyGaraOto.Properties.Resources.CarIs;
+            this.pictureBox1.Location = new System.Drawing.Point(511, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(423, 365);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Loichucdau
+            // 
+            this.Loichucdau.AutoSize = true;
+            this.Loichucdau.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Loichucdau.Location = new System.Drawing.Point(650, 426);
+            this.Loichucdau.Name = "Loichucdau";
+            this.Loichucdau.Size = new System.Drawing.Size(156, 28);
+            this.Loichucdau.TabIndex = 2;
+            this.Loichucdau.Text = "Chào buổi sáng";
+            this.Loichucdau.Click += new System.EventHandler(this.Loichucdau_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 45);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Gara oto vui vẻ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // UserNameText
+            // 
+            this.UserNameText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UserNameText.Location = new System.Drawing.Point(50, 90);
+            this.UserNameText.Name = "UserNameText";
+            this.UserNameText.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.ProfessionalSystem;
+            this.UserNameText.Size = new System.Drawing.Size(266, 34);
+            this.UserNameText.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.UserNameText.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.UserNameText.StateCommon.Border.Rounding = 12;
+            this.UserNameText.StateCommon.Content.Padding = new System.Windows.Forms.Padding(5, 2, 5, 2);
+            this.UserNameText.TabIndex = 4;
+            // 
+            // PassWordText
+            // 
+            this.PassWordText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PassWordText.Location = new System.Drawing.Point(50, 147);
+            this.PassWordText.Name = "PassWordText";
+            this.PassWordText.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.ProfessionalSystem;
+            this.PassWordText.PasswordChar = '•';
+            this.PassWordText.Size = new System.Drawing.Size(265, 34);
+            this.PassWordText.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.PassWordText.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.PassWordText.StateCommon.Border.Rounding = 12;
+            this.PassWordText.StateCommon.Content.Padding = new System.Windows.Forms.Padding(5, 2, 5, 2);
+            this.PassWordText.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.SigninButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.PassWordText);
+            this.panel1.Controls.Add(this.UserNameText);
+            this.panel1.Location = new System.Drawing.Point(27, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(382, 343);
+            this.panel1.TabIndex = 6;
+            // 
+            // SigninButton
+            // 
+            this.SigninButton.Location = new System.Drawing.Point(100, 209);
+            this.SigninButton.Name = "SigninButton";
+            this.SigninButton.Size = new System.Drawing.Size(160, 35);
+            this.SigninButton.StateCommon.Back.Color1 = System.Drawing.Color.DeepSkyBlue;
+            this.SigninButton.StateCommon.Back.Color2 = System.Drawing.Color.DeepSkyBlue;
+            this.SigninButton.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.SigninButton.StateCommon.Border.Rounding = 12;
+            this.SigninButton.StateNormal.Content.ShortText.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SigninButton.StatePressed.Back.Color1 = System.Drawing.Color.DodgerBlue;
+            this.SigninButton.StatePressed.Back.Color2 = System.Drawing.Color.DodgerBlue;
+            this.SigninButton.StatePressed.Content.ShortText.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SigninButton.StateTracking.Back.Color1 = System.Drawing.Color.DodgerBlue;
+            this.SigninButton.StateTracking.Back.Color2 = System.Drawing.Color.DodgerBlue;
+            this.SigninButton.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.SigninButton.StateTracking.Border.Rounding = 12;
+            this.SigninButton.TabIndex = 6;
+            this.SigninButton.Values.Text = "Đăng nhập";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1002, 513);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Loichucdau);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "LoginForm";
+            this.Palette = this.kryptonPalette1;
+            this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label Loichucdau;
+        private System.Windows.Forms.Label label1;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox UserNameText;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox PassWordText;
+        private System.Windows.Forms.Panel panel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton SigninButton;
+    }
+}
+
---
+++ PhanMemQuanLyGaraOto/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace PhanMemQuanLyGaraOto
+{
+    public partial class LoginForm : KryptonForm
+    {
+
+        public LoginForm()
+        {
+            InitializeComponent();
+            LoiChucInit();
+            UserNameText.GotFocus += UserNameText_GotFocus;
+            UserNameText.LostFocus += UserNameText_LostFocus;
+            SigninButton.Text = "Đăng nhập";
+        }
+
+        private void UserNameText_LostFocus(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(UserNameText.Text))
+                UserNameText.Text = "UserName";
+        }
+
+        private void UserNameText_GotFocus(object sender, EventArgs e)
+        {
+            if (UserNameText.Text == "Tài khoản")
+            {
+                UserNameText.Text = "";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonPalette1_PalettePaint(object sender, PaletteLayoutEventArgs e)
+        {
+
+        }
+
+        void LoiChucInit()
+        {
+            TimeSpan timeSpan = DateTime.Now.TimeOfDay;
+            if (timeSpan.Hours > 3 && timeSpan.Hours < 11)
+                Loichucdau.Text = "Chào buổi sáng";
+            else if (timeSpan.Hours >= 11 && timeSpan.Hours < 13)
+                Loichucdau.Text = "chào buổi trưa";
+            else if (timeSpan.Hours >= 13 && timeSpan.Hours < 19)
+                Loichucdau.Text = "chào buổi chiều";
+            else if (timeSpan.Hours >= 19 && timeSpan.Hours <23)
+                Loichucdau.Text = "Chào buổi tối";
+            else
+                Loichucdau.Text = "Chúc ngủ ngon";
+
+        }
+
+        private void Loichucdau_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
